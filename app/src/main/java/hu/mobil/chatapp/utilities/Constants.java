ccb0aae2d33ff73f63903b5d28660992f533bc2d
Slavcev Uros (h985232) <urosslavcev@gmail.com>
2022-05-04 09:42:24 +0200

    login done

---
+++ app/src/main/java/hu/mobil/chatapp/utilities/Constants.java
@@ -0,0 +1,12 @@
+package hu.mobil.chatapp.utilities;
+
+public class Constants {
+    public static final String KEY_COLLECTION_USERS = "felhasználók";
+    public static final String KEY_NAME = "név";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "jelszó";
+    public static final String KEY_PREFERENCE_NAME = "chatAppPreference";
+    public static final String KEY_IS_SIGNED_IN = "bejelentkezve";
+    public static final String KEY_USER_ID = "felhasználóId";
+    public static final String KEY_IMAGE = "kép";
+}
